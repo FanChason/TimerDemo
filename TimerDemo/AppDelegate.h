54e1947e238a65712301ec6f306e65ef1c1a8b5c
fanxc <fanxc@morningtec.cn>
2016-12-26 17:21:33 +0800

    Initial Commit

---
+++ TimerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimerDemo
+//
+//  Created by  czk on 16/12/26.
+//  Copyright © 2016年 fanxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
